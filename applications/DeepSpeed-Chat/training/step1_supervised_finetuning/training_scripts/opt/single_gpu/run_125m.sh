c0f547e70607637647e5b4dbe6f69883000177b1
daliu <liudaax@126.com>
2023-12-13 17:44:09 +0800

    调整

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/opt/single_gpu/run_125m.sh
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/opt/single_gpu/run_125m.sh
@@ -16,8 +16,8 @@ fi
 mkdir -p $OUTPUT
 
 deepspeed --num_gpus 1 main.py --model_name_or_path facebook/opt-125m \
-   --per_device_train_batch_size 8 \
-   --gradient_accumulation_steps 8 --lora_dim 128 --zero_stage $ZERO_STAGE \
+   --per_device_train_batch_size 4 \
+   --gradient_accumulation_steps 16 --lora_dim 128 --zero_stage $ZERO_STAGE \
    --enable_tensorboard \
    --tensorboard_path $OUTPUT \
    --deepspeed --output_dir $OUTPUT &> $OUTPUT/training.log
